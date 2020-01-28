789b0507215fc33ad1012152fb6a84d803885986
Daniel Pumayauli <danielpumayauli@gmail.com>
2020-01-28 16:32:16 -0500

    Updates

---
+++ precache-manifest.4d66d80e3a48d64653990665d299c1de.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e30c14d74e74334c8299ac258da2f2c1",
+    "url": "/lms/index.html"
+  },
+  {
+    "revision": "26d48bdeca133658eecc",
+    "url": "/lms/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "b98fd01bc3739ea196d1",
+    "url": "/lms/static/css/main.707230ce.chunk.css"
+  },
+  {
+    "revision": "26d48bdeca133658eecc",
+    "url": "/lms/static/js/2.8d46e37a.chunk.js"
+  },
+  {
+    "revision": "b98fd01bc3739ea196d1",
+    "url": "/lms/static/js/main.16ffb3f6.chunk.js"
+  },
+  {
+    "revision": "d9bcbd8cfac96e22a40e",
+    "url": "/lms/static/js/runtime-main.57b9f76c.js"
+  },
+  {
+    "revision": "e83e44099816996c3f564ae93b1290dd",
+    "url": "/lms/static/media/1.e83e4409.jpg"
+  },
+  {
+    "revision": "6c5c9f2c1a687d9c0af7f7c65f8dd676",
+    "url": "/lms/static/media/3.6c5c9f2c.png"
+  },
+  {
+    "revision": "e83a404f914937547b0688b7a5ab4acb",
+    "url": "/lms/static/media/4.e83a404f.png"
+  },
+  {
+    "revision": "09640ee7ed691cabfc1a3bb87fb3230b",
+    "url": "/lms/static/media/bg-login.09640ee7.jpg"
+  },
+  {
+    "revision": "3965e599aa19ca8e8d726dbcfea53aa7",
+    "url": "/lms/static/media/curso2.3965e599.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/lms/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "acf3dcb7ff752b5296ca23ba2c7c2606",
+    "url": "/lms/static/media/fontawesome-webfont.acf3dcb7.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/lms/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/lms/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/lms/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
